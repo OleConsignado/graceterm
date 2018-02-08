b3706c7b63f1def8ad04552f542598d534cc1baa
Matheus Neder <matheusneder@gmail.com>
2018-02-08 14:10:32 -0200

    Code reorganize

---
+++ Source/Graceterm.Tests/AfterStopRequestTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Graceterm.Tests
+{
+    public class AfterStopRequestTests
+    {
+        [Fact]
+        public async Task ShouldReturnServiceUnavailableForResquestCreatedAfterStopRequest()
+        {
+            var server = Server.Create();
+            server.CreateRequests(1); // Create a request before ask to terminate in order to hold server up
+
+            await Task.Delay(1000); // Ensure that requests will be all submitted
+
+            // simulate sigterm
+            var stopTask = Task.Run(() =>
+            {
+                server.Stop();
+            });
+
+            await Task.Delay(10); // Ensure the stopTask will run
+
+            var result = await server.CreateRequest();
+            Assert.Equal(HttpStatusCode.ServiceUnavailable, result.StatusCode);
+            await stopTask;
+        }
+    }
+}
--- Source/Graceterm.Tests/Server.cs
+++ Source/Graceterm.Tests/Server.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -8,7 +7,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 using System.Collections.Generic;
-using System.Net.Sockets;
 using Microsoft.AspNetCore.TestHost;
 
 namespace Graceterm.Tests
@@ -16,12 +14,7 @@ namespace Graceterm.Tests
     public class Server
     {
         public const string ResponseContent = "hello";
-        //private readonly int port;
-        //private const string ip = "localhost";
-        public TestServer _testServer;
-
-        //public Task ServerTask { get; set; }
-
+        private TestServer _testServer;
         private IApplicationLifetime _applicationLifetime;
 
         public Task<HttpResponseMessage> CreateRequest()
@@ -29,10 +22,7 @@ namespace Graceterm.Tests
             var requestMessage = new HttpRequestMessage(new HttpMethod("GET"), "/");
 
             return Task.Factory.StartNew(() =>
-                    //new HttpClient() { BaseAddress = new Uri($"http://localhost:{port}") }
-                    //    .SendAsync(requestMessage).Result
-                    _testServer.CreateClient().GetAsync("/").Result
-                        ,
+                    _testServer.CreateClient().GetAsync("/").Result,
                 TaskCreationOptions.LongRunning);
         }
 
@@ -65,10 +55,7 @@ namespace Graceterm.Tests
 
         protected Server(GracetermOptions gracetermOptions)
         {
-            //port = FreeTcpPort();
-
             var webHostBuilder = new WebHostBuilder()
-                //.UseKestrel(o => o.Listen(IPAddress.Parse("0.0.0.0"), port))
                 .ConfigureLogging(loggingBuilder =>
                 {
                     loggingBuilder.AddDebug();
@@ -99,25 +86,7 @@ namespace Graceterm.Tests
                 }
             );
 
-            //ServerTask = Task.Factory.StartNew(() =>
-            //{
-            //    using (var webHost = webHostBuilder.Build())
-            //    {
-            //        webHost.Run();
-            //    }
-            //}, TaskCreationOptions.LongRunning);
-
             _testServer = new TestServer(webHostBuilder);
         }
-
-        // based on https://stackoverflow.com/questions/138043/find-the-next-tcp-port-in-net
-        //private static int FreeTcpPort()
-        //{
-        //    TcpListener l = new TcpListener(IPAddress.Loopback, 0);
-        //    l.Start();
-        //    int port = ((IPEndPoint)l.LocalEndpoint).Port;
-        //    l.Stop();
-        //    return port;
-        //}
     }
 }

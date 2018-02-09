d734ae7614b53bd59bf7aa5eff10743baa4ca4d0
Matheus Neder <matheusneder@gmail.com>
2018-02-09 13:47:38 -0200

    Fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,11 @@ then
 	SUFFIX_ARG="--version-suffix=p$TRAVIS_BRANCH-b$TRAVIS_BUILD_NUMBER"
 fi
 
+echo "SUFFIX_ARG: $SUFFIX_ARG"
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_BRANCH^^: ${TRAVIS_BRANCH^^}"
+
 dotnet pack -c Release $SUFFIX_ARG -o $ARTIFACTS_FOLDER
-dotnet nuget push --api-key $NUGET_API_KEY $ARTIFACTS_FOLDER/*.nupkg --source https://api.nuget.org/v3/index.json
+#dotnet nuget push --api-key $NUGET_API_KEY $ARTIFACTS_FOLDER/*.nupkg --source https://api.nuget.org/v3/index.json
 
 rm -Rf $ARTIFACTS_FOLDER
